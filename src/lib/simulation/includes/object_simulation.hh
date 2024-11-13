674c3de137a2d944505ad0e551c08f2463aaafcb
Vicent Giménez Alventosa <vicent.gimenez.alventosa@gmail.com>
2024-11-13 09:52:28 +0100

    Update version number

--- src/lib/simulation/includes/object_simulation.hh
+++ src/lib/simulation/includes/object_simulation.hh
@@ -686,7 +686,7 @@ namespace penred{
       static std::string versionMessage(){
 
 	return std::string("***************************************************************\n"
-			   " PenRed version: 1.12.0 (25-September-2024) \n"
+			   " PenRed version: 1.12.0b (13-November-2024) \n"
 			   " Copyright (c) 2019-2024 Universitat Politecnica de Valencia\n"
 			   " Copyright (c) 2019-2024 Universitat de Valencia\n"
 			   " Reference: Computer Physics Communications, 267 (2021) 108065\n"
